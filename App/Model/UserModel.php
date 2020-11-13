fe39c09820585bf327bee45925c4cf54c3a54866
Tom <chewie.louveau@gmail.com>
2020-11-13 19:19:16 +0100

    update usermodel

--- App/Model/UserModel.php
+++ App/Model/UserModel.php
@@ -4,11 +4,11 @@ namespace App\Model;
 use Core\Database;
 
 class userModel extends Database{
-    
+
 
     public function getUsers()
     {
-        $query = $this->pdo->query("SELECT * FROM orders");
+        $query = $this->pdo->query("SELECT * FROM users");
         return $query->fetchAll(\PDO::FETCH_OBJ);
     }
 }
